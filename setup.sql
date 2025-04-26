d8f580307c6b4d1721eb602d7ab7199eb29945cd
Nithyanandam Thangamuthu <36470262+nithyancse@users.noreply.github.com>
2025-04-26 16:10:08 +0530

    create table query

--- setup.sql
+++ setup.sql
@@ -4,3 +4,45 @@ insert into user (email_id, password, created_at, updated_at) value('nithyanmobi
 update user set name="Nithyanandam T", email_id = '1', password="1" , status="A" where id=1;
 update user set name="Anand"  where id=2;
 
+
+
+
+
+/*DROP TABLE IF EXISTS `menu`;*/;
+CREATE TABLE `menu` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `status` varchar(2) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `user_id` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+CREATE TABLE `topic` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `description` text COLLATE utf8_unicode_ci NOT NULL,
+  `menu_id` bigint(20) DEFAULT NULL,
+  `status` varchar(2) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `title` varchar(500) COLLATE utf8_unicode_ci NOT NULL,
+  `type` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=643 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+CREATE TABLE `user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `email_id` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  `name` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `password` varchar(60) COLLATE utf8_unicode_ci NOT NULL,
+  `status` varchar(2) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
